311aad570765bfde03c28ef34f0db9afb3d88258
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-09-11 21:58:21 -0700

    Added new unit test to check specific data.

--- server/test/Program.test.js
+++ server/test/Program.test.js
@@ -1,32 +1,32 @@
 require('../config/config');
+const moment = require('moment');
 const expect = require('expect');
 const { db, ExecuteNonQuery } = require('../database/Database');
 const { AddNewProgram, GetProgramData } = require('../database/Program');
 
 describe('Database program table...', () => {
+    const sDate = new Date().getTime();
+    const eDate = new Date(moment()).getTime() + 647340000;     // 7d 11h 49m
 
     before(() => {
         db.serialize(() => {
             ExecuteNonQuery('DELETE FROM Program');
         });
     });
-    
-    it('should be empty...', (done) => {
-        GetProgramData()
-            .then(val => {
-                expect(val).toHaveLength(0);
-                done();
-            })
-            .catch((e) => done(e));
+
+    it('should be empty...', async () => {
+        const val = await GetProgramData();
+        expect(val).toHaveLength(0);
     });
 
-    it('should have one (1) entry...', (done) => {
-        AddNewProgram('Test Program 1', new Date().getTime(), new Date().getTime());
-        GetProgramData()
-            .then(val => {
-                expect(val).toHaveLength(1);
-                done();
-            })
-            .catch((e) => done(e));
+    it('should have one (1) entry...', async () => {
+        AddNewProgram('Test Program 1', sDate, eDate);
+        const val = await GetProgramData();
+        expect(val).toHaveLength(1);
+    });
+
+    it(`should contain ${eDate}, Test Program 1, ${sDate}...`, async () => {
+        const val = await GetProgramData();
+        expect(val).toMatchObject([{name:'Test Program 1', startDateTime: sDate, endDateTime: eDate}]);
     });
 });
