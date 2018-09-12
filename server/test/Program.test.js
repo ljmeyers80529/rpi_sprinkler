c48d27e3e72143e83d9ac2bd42b56bac3899492f
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-09-11 21:11:59 -0700

    Unit test for Program data table.

--- server/test/Program.test.js
+++ server/test/Program.test.js
@@ -1,10 +1,32 @@
 require('../config/config');
 const expect = require('expect');
+const { db, ExecuteNonQuery } = require('../database/Database');
 const { AddNewProgram, GetProgramData } = require('../database/Program');
 
-describe('Database program...', () => {
-    const tv = 100;
-    it('should be a number...', () => {
-        expect(tv).toBe(100);
+describe('Database program table...', () => {
+
+    before(() => {
+        db.serialize(() => {
+            ExecuteNonQuery('DELETE FROM Program');
+        });
+    });
+    
+    it('should be empty...', (done) => {
+        GetProgramData()
+            .then(val => {
+                expect(val).toHaveLength(0);
+                done();
+            })
+            .catch((e) => done(e));
+    });
+
+    it('should have one (1) entry...', (done) => {
+        AddNewProgram('Test Program 1', new Date().getTime(), new Date().getTime());
+        GetProgramData()
+            .then(val => {
+                expect(val).toHaveLength(1);
+                done();
+            })
+            .catch((e) => done(e));
     });
 });
