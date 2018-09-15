5623adeaa8a07f82c37d23ea22b84da7594d75d8
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-09-14 23:07:51 -0700

    Added addition tests.

--- server/test/Program.test.js
+++ server/test/Program.test.js
@@ -1,9 +1,10 @@
 require('../config/config');
 const moment = require('moment');
 const expect = require('expect');
-const { db, ExecuteNonQuery } = require('../database/Database');
-const { AddNewProgram, GetProgramData } = require('../database/Program');
+const { db, ExecuteNonQuery, ExecuteScalar } = require('../database/Database');
+const { AddNewProgram, GetProgramData, GetProgramEntry, UpdateProgram } = require('../database/Program');
 const utils = require('../utils/Utils');
+const { LastRowID } = require('../database/Common');
 
 describe('Database program table...', () => {
     let sDate = new Date().getTime();
@@ -31,5 +32,20 @@ describe('Database program table...', () => {
         expect(val).toMatchObject([{name:'Test Program 1', startDateTime: sDate, endDateTime: eDate}]);
     });
 
-    // it(`should have specific entry `)
+    it(`should have specific entry: ${moment(sDate).local()}, Test Program 2, ${moment(eDate).local()}...`, async () => {
+        AddNewProgram('Test Program 2', sDate, eDate);
+        const lrId = await LastRowID();
+        const val = await GetProgramEntry(lrId);
+        expect(val).toMatchObject({name:'Test Program 2', startDateTime: sDate, endDateTime: eDate});
+    });
+
+    it(`should properly update an entry...`, async () => {
+        let val = await GetProgramData();
+        expect(val.length >=2 ).toBeTruthy();
+        let element = val[1];
+        const id = element.id;
+        await UpdateProgram(id, 'Program Updated', element.startDateTime, element.endDateTime);
+        val = await GetProgramData();
+        expect(val[1]).toMatchObject({name:'Program Updated', startDateTime: sDate, endDateTime: eDate});
+    });
 });
