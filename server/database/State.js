62fed6cb4d629e4b25d7097ba43a93ad955876cb
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-09-27 22:40:38 -0700

    Extended State data table access functions.

--- server/database/State.js
+++ server/database/State.js
@@ -1,12 +1,13 @@
 const { ExecuteNonQuery, ExecuteScalar } = require('./Database');
 
 const GetState = () => {
-    const getData = `SELECT rainDelay, useWeatherConditions, zeroRunTemperature, autoRun
-                     FROM state`;
+    const getData = `SELECT rainDelay, useWeatherConditions, zeroRunTemperature, autoRun,
+                       location, latitude, longitude
+                     FROM State`;
     return ExecuteScalar(getData);
 }
 
-const UpdateState = (rainDelay, zeroRun, useWeather, autoRun) => {
+const UpdateStateWeather = (rainDelay, zeroRun, useWeather, autoRun) => {
     const upd = `UPDATE State SET
                  rainDelay = ${rainDelay},
                  zeroRunTemperature = ${zeroRun},
@@ -15,4 +16,12 @@ const UpdateState = (rainDelay, zeroRun, useWeather, autoRun) => {
     return ExecuteNonQuery(upd);
 };
 
-module.exports = { UpdateState, GetState };
+const UpdateStateLocation = (location, latitude, longitude) => {
+    const upd = `UPDATE State SET
+                 location = '${location}',
+                 latitude = ${latitude},
+                 longitude = ${longitude}`;
+    return ExecuteNonQuery(upd);
+};
+
+module.exports = { UpdateStateWeather, GetState, UpdateStateLocation };
