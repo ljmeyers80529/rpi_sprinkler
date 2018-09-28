1dc5d1c8c47649b8c9fe833d547666113b18efbe
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-09-27 22:41:40 -0700

    Extended tests for State data table.

--- server/test/State.test.js
+++ server/test/State.test.js
@@ -7,22 +7,32 @@ const utils = require('../utils/Utils');
 describe('Database state table...', () => {
     const rainTime = utils.dateTimeOffset(new Date().getTime(), 0, 11, 39);
 
-    it('should update State data table...', async () => {
+    it('should update State data table - weather...', async () => {
         const preset = {rainDelay: 0, zeroRunTemperature: 50, useWeatherConditions: 1, autorun: 0 };
-        const updated = {rainDelay: new Date(rainTime).getTime(), zeroRunTemperature: 43, useWeatherConditions: 0, autorun: 1}
+        const wUpd = {rainDelay: new Date(rainTime).getTime(), zeroRunTemperature: 43, useWeatherConditions: 0, autorun: 1};
 
         let cState = await state.GetState();
         expect(cState).toMatchObject(preset);
-        await state.UpdateState(updated.rainDelay, updated.zeroRunTemperature, updated.useWeatherConditions, updated.autorun);
+        await state.UpdateStateWeather(wUpd.rainDelay, wUpd.zeroRunTemperature, wUpd.useWeatherConditions, wUpd.autorun);
         cState = await state.GetState();
-        expect(cState).toMatchObject(updated);
+        expect(cState).toMatchObject(wUpd);
     });
 
-    after(() => {
-        database.db.serialize(() => {
-            state.UpdateState(0, 50, true, false);
-        });
+    it('should update State data table - location...', async () => {
+        const preset = {location: '', latitude: 0.0, longitude: 0.0 };
+        const lUpd = {location: 'Fresno, CA', latitude: 36.7377981, longitude: -119.7871247 };
+
+        let cState = await state.GetState();
+        expect(cState).toMatchObject(preset);
+        await state.UpdateStateLocation(lUpd.location, lUpd.latitude, lUpd.longitude);
+        cState = await state.GetState();
+        expect(cState).toMatchObject(lUpd);
     });
+
+    after( async () => {
+        await state.UpdateStateWeather(0, 50, true, false);
+        await state.UpdateStateLocation('', 0.0, 0.0);
+        });
 });
 
 
